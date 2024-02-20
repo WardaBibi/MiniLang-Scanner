fcbfcb43bc480b3904daf91895ab23662d269e73
WardaBibi <codewithwarda@gmail.com>
2024-02-20 18:54:55 +0500

    Added Scanner

---
+++ Scanner/config.c
@@ -0,0 +1,9 @@
+
+
+  x = x + 5;
+    y = x - 3;
+    if (flag != false) {
+        print("Flag is true");
+    } else {
+        print("Flag is false");
+    }
---
+++ Scanner/samples/code1.c
@@ -0,0 +1,11 @@
+int x = 10;
+    int y = 20;
+    bool flag = true;
+    if (x == y) {
+        print("x is equal to y");
+    } else {
+        print("x is not equal to y");
+    }
+    // This is a single line comment
+
+    
